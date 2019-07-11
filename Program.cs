8001d81e033d42a25cdb84af2dffd7a1809243ae
Kyle Todd <Kyletodd119@gmail.com>
2019-07-11 16:50:18 -0600

    help, quit, reset, user input working working

--- Program.cs
+++ Program.cs
@@ -8,9 +8,26 @@ namespace CastleGrimtol
     public static void Main(string[] args)
     {
       Console.Clear();
-      System.Console.WriteLine("You awake from Hypersleep suddenly. You can see the lights are flickering above you and the air smells stale. All the other pods are already open and you smell a strange aroma. You can tell something must be wrong with the ship. You get dressed and see there is a door to your left. Where do you choose to go?");
-      Console.ReadLine();
-      G
+      System.Console.Write(@"
+                           |-----------|
+           i               |===========|                       
+           |               |,---------.|                      __--~\__--.
+    #---,'""""`-_   `n     |`---------'|    `n    `n     ,--~~  __-/~~--'_____.
+       |~~~~~~~~~|---~---/=|___________|=\---~-----~-----| .--~~  |  .__|     |
+     -[|.--_. ===|#####|-| |@@@@|+-+@@@| |]=###|/-++++-[| ||||___+_.  | `===='-.
+     -[|'==~'    |#####|-| |@@@@|+-+@@@| |]=###|\-++++-[| ||||~~~+~'  | ,====.-'
+       |_________|---u---\=|~~~~~~~~~~~|=/---u-----u-----| '--__  |  '~~|     |
+        \       /=-   `    |,---------.|      `     `    `--__  ~~-\__--.~~~~~'
+----=:===\     /           |`---------'|                      ~~--_/~~--'
+      --<:\___/--          |===========|
+                           |-----------|
+                           |___________|");
+      System.Console.WriteLine();
+      System.Console.Write("You awake from Hypersleep suddenly. You can see the lights are flickering above you,and the air smells foul. All the other pods are already open, you must be the last to wake. How is this when you are the captain? You can tell something must be wrong. You need to get to the bridge located at the northern most part of the ship to see what is wrong with the electrical system and check your course. You get dressed and notice your nametag says: ");
+      string name = Console.ReadLine();
+      GameService gameService = new GameService();
+      gameService.StartGame();
+
     }
   }
 }
--- Project/GameService.cs
+++ Project/GameService.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Threading;
 using CastleGrimtol.Project.Interfaces;
 using CastleGrimtol.Project.Models;
 
@@ -6,22 +8,125 @@ namespace CastleGrimtol.Project
 {
   public class GameService : IGameService
   {
-    public Room CurrentRoom { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-    public Player CurrentPlayer { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public bool Running = true;
+    public Room CurrentRoom { get; set; }
+    public Player CurrentPlayer { get; set; }
 
+
+    public void Setup()
+    {
+      //create all my rooms
+      Room hypersleep = new Room("The Hypersleep Chamber. Back of the ship.", "You see the door to the cafeteria to the west, the viewport on the door is smeared with blood.");
+      Room cafeteria = new Room("The Cafeteria. Back of the ship", "The door slides sideways to allow your entry. There are bloodstains everywhere, but not a single body to be found. The lights continue to flicker. There is evidence of a struggle with turned over tables, scorchmarks, and small unexplained holes burned through the floor. There are two streaks of blood headed west to the door to the living quarters. The door seems to be stuck in some kind of loop opening partially and closing. inidicating something or someone was either dragged or crawled to that door. The door to the north leads to the main hallway leading closer to the bridge. The view through the viewport is dark.");
+      Room livingQuarters = new Room("The Living Quarters. Middle of the ship", "fill in later");
+      Room hall = new Room("", "");
+      Room engine = new Room("", "");
+      Room science = new Room("", "");
+      Room armory = new Room("", "");
+      Room bridge = new Room("", "");
+
+
+      //set up their relationships
+      hypersleep.Exits.Add("west", cafeteria);
+      cafeteria.Exits.Add("west", livingQuarters);
+      cafeteria.Exits.Add("north", hall);
+      engine.Exits.Add("west", hall);
+      engine.Exits.Add("north", science);
+      livingQuarters.Exits.Add("east", hall);
+      livingQuarters.Exits.Add("north", armory);
+      armory.Exits.Add("east", science);
+      armory.Exits.Add("north", bridge);
+
+      Item flamethrower = new Item("Flamethrower", "Keep out of reach of children");
+      armory.Items.Add(flamethrower);
+
+
+
+
+
+
+      CurrentRoom = hypersleep;
+    }
     public void GetUserInput()
     {
-      throw new System.NotImplementedException();
+      string[] choice = Console.ReadLine().ToLower().Split(" ");
+      string command = choice[0];
+      string dir = "";
+      if (choice.Length > 1)
+      {
+        dir = choice[1];
+
+      }
+      switch (command)
+      {
+        case "go":
+          Go(dir);
+          break;
+
+        case "help":
+          Help();
+          break;
+
+        case "inventory":
+          Inventory();
+          break;
+
+        case "look":
+          Look();
+          break;
+
+        case "quit":
+          Quit();
+          break;
+
+        case "reset":
+          Reset();
+          break;
+
+        case "take":
+          TakeItem(dir);
+          break;
+
+        case "use":
+          UseItem(dir);
+          break;
+
+        default:
+          System.Console.WriteLine("Not Possible");
+          break;
+      }
     }
 
-    public void Go(string direction)
+    public void Go(string input)
     {
-      throw new System.NotImplementedException();
+
     }
 
     public void Help()
     {
-      throw new System.NotImplementedException();
+      System.Console.WriteLine(@"
+ ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄            ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌
+▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌          ▐░█▀▀▀▀▀▀▀█░▌
+▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌       ▐░▌
+▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌          ▐░█▄▄▄▄▄▄▄█░▌
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌
+▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌          ▐░█▀▀▀▀▀▀▀▀▀ 
+▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌          
+▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌          
+▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌          
+ ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀          
+      While playing the game you may use the following commands:
+      Go - type go and one of the compass directions to move between rooms
+      Help - brings you here, same in every room
+      Inventory - check the items currently available in your inventory
+      Look - look around the room
+      Quit - exit the game in fear
+      Reset - reset the game
+      Take - in a room with an available item, add that item to your inventory
+      use - use item in your inventory
+
+      ");
     }
 
     public void Inventory()
@@ -31,28 +136,77 @@ namespace CastleGrimtol.Project
 
     public void Look()
     {
-      throw new System.NotImplementedException();
+      Console.Clear();
+      System.Console.Write($"{CurrentRoom.Description}");
     }
 
     public void Quit()
     {
-      throw new System.NotImplementedException();
+      System.Console.WriteLine(@"
+ ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌ ▀▀▀▀█░█▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ 
+▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌          ▐░▌     
+▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌          ▐░▌     
+▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌          ▐░▌     
+▐░█▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌     ▐░▌          ▐░▌     
+▐░░░░░░░░░░░▌▐░▌       ▐░▌     ▐░▌          ▐░▌     
+ ▀▀▀▀▀▀█░█▀▀ ▐░█▄▄▄▄▄▄▄█░▌ ▄▄▄▄█░█▄▄▄▄      ▐░▌     
+        ▐░▌  ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌     
+         ▀    ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀      
+      Are you sure you want to quit the current game? You will be launched into the void of space. (Y/N)?");
+      if (Console.ReadLine() == "y".ToLower())
+      {
+        Running = false;
+      }
+      else
+      {
+        Look();
+      }
     }
 
     public void Reset()
     {
-      throw new System.NotImplementedException();
+      System.Console.WriteLine(@"
+ ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ 
+▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌               ▐░▌     
+▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄      ▐░▌     
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌     
+▐░█▀▀▀▀█░█▀▀ ▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀      ▐░▌     
+▐░▌     ▐░▌  ▐░▌                    ▐░▌▐░▌               ▐░▌     
+▐░▌      ▐░▌ ▐░█▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄      ▐░▌     
+▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌     
+ ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀      
+Are you sure you wish to start over from the beginning?");
+      if (Console.ReadLine() == "y".ToLower())
+      {
+        Setup();
+        StartGame();
+
+      }
+      else
+      {
+        Look();
+      }
     }
 
-    public void Setup()
-    {
-      throw new System.NotImplementedException();
-    }
 
     public void StartGame()
     {
-      throw new System.NotImplementedException();
+      while (Running)
+      {
+        Console.Clear();
+        System.Console.WriteLine($"{CurrentRoom.Description}");
+        GetUserInput();
+
+      }
+
+
+
     }
+
 
     public void TakeItem(string itemName)
     {
@@ -62,6 +216,11 @@ namespace CastleGrimtol.Project
     public void UseItem(string itemName)
     {
       throw new System.NotImplementedException();
+    }
+    public GameService()
+    {
+      Setup();
+
     }
   }
 }
