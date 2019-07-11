d4a0d7744cd7dc8520484c4c7f681c604b6c4c69
Kyle Todd <Kyletodd119@gmail.com>
2019-07-11 11:33:34 -0600

    initialize

--- Program.cs
+++ Program.cs
@@ -3,11 +3,14 @@ using CastleGrimtol.Project;
 
 namespace CastleGrimtol
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-
-        }
+      Console.Clear();
+      System.Console.WriteLine("You awake from Hypersleep suddenly. You can see the lights are flickering above you and the air smells stale. All the other pods are already open and you smell a strange aroma. You can tell something must be wrong with the ship. You get dressed and see there is a door to your left. Where do you choose to go?");
+      Console.ReadLine();
+      G
     }
+  }
 }
